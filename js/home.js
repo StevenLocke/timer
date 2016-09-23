27d91c92ec4c44c9b4a14898f3647da4e0f024e4
Steven Locke <s3locke@uwaterloo.ca>
2016-09-22 18:53:06 -0700

    Added pause button to the timer section.

--- js/home.js
+++ js/home.js
@@ -6,6 +6,8 @@
 
 var gTimer;
 var gCurrentAlarmTime;
+var gRemainingTime;
+var gPaused = false;
 
 window.addEventListener('load', function() {
     document.getElementById('inputDiv').style.display = 'block';
@@ -17,7 +19,7 @@ window.addEventListener('load', function() {
         clearInputs();
     });
 
-    var relativeInput = document.getElementById('inputVal');
+    var relativeInput = document.getElementById('relativeInputField');
     relativeInput.addEventListener('keypress', function(e) {
         var key = e.which || e.keyCode;
         if(key == 13) {
@@ -39,7 +41,7 @@ window.addEventListener('load', function() {
         clearInputs();
     });
 
-    var exactInput = document.getElementById('inputDateVal');
+    var exactInput = document.getElementById('exactInputField');
     exactInput.addEventListener('keypress', function(e) {
         var key = e.which || e.keyCode;
         if(key == 13) {
@@ -57,18 +59,23 @@ window.addEventListener('load', function() {
     snoozeButton.addEventListener('click', function() {
         snoozeTimer();
     });
+
+    var pauseResumeButton = document.getElementById('pauseResumeButton');
+    pauseResumeButton.addEventListener('click', function() {
+        pauseResumeTimer();
+    });
 });
 
 function clearInputs() {
-    var relative = document.getElementById('inputVal');
-    var exact = document.getElementById('inputDateVal');
+    var relative = document.getElementById('relativeInputField');
+    var exact = document.getElementById('exactInputField');
 
     relative.value = "";
     exact.value = "";
 }
 
 function startRelativeTimer() {
-    var input = document.getElementById('inputVal').value;
+    var input = document.getElementById('relativeInputField').value;
 
     //Match any number of digits, followed by either one or zero (a literal . or : followed by any number of digits)
     var regEx = /^(\d*)(?:([\.:])(\d*))?$/;
@@ -132,7 +139,7 @@ function startExactTimer() {
     }
 
 
-    var timeInput = (document.getElementById('inputDateVal').value);
+    var timeInput = (document.getElementById('exactInputField').value);
     var time = parseTime(timeInput);
 
     if (timeInput != "" && !isNaN(time.getTime())) {
@@ -152,13 +159,12 @@ function displayTime(dateTime) {
     var messageSpan = document.getElementById('messageSpan');
     messageSpan.innerHTML = "";
 
-    var now = new Date();
-
     var message = document.getElementById('alertMessage');
     if (message.value != "") {
         messageSpan.innerHTML = message.value;
     }
 
+    var now = new Date();
     var endTimeSpan = document.getElementById('endTimeSpan');
     endTimeSpan.innerHTML = "End time: ";
     endTimeSpan.innerHTML += (dateTime.getHours() > 12 ? dateTime.getHours() - 12 : dateTime.getHours()) + ":";
@@ -166,48 +172,58 @@ function displayTime(dateTime) {
     endTimeSpan.innerHTML += padZeroes(dateTime.getSeconds()) + (dateTime.getHours() > 12 ? " PM" : " AM");
 
     gTimer = setInterval(function() {
-        if (now < dateTime) {
-            var difference = dateTime - now;
-            difference = new Date(difference);
-            var remainingTimeString = "";
-            var titleSting = "";
-            if (difference.getUTCHours() > 0) {
-                remainingTimeString += difference.getUTCHours();
-                remainingTimeString += " hours, ";
-                titleSting += difference.getUTCHours();
-                titleSting += "h ";
+        if (!gPaused) {
+            if (now < dateTime) {
+                var difference = dateTime - now;
+                difference = new Date(difference);
+                var remainingTimeString = "";
+                var titleSting = "";
+                if (difference.getUTCHours() > 0) {
+                    remainingTimeString += difference.getUTCHours();
+                    remainingTimeString += " hours, ";
+                    titleSting += difference.getUTCHours();
+                    titleSting += "h ";
+                }
+                remainingTimeString += difference.getUTCMinutes();
+                remainingTimeString += " minutes, ";
+                titleSting += difference.getUTCMinutes();
+                titleSting += "m ";
+
+                remainingTimeString += difference.getUTCSeconds();
+                remainingTimeString += " seconds";
+                titleSting += difference.getUTCSeconds();
+                titleSting += "s";
+
+                document.title = titleSting;
+                timerSpan.innerHTML = "Remaining time: " + remainingTimeString;
+                now.setSeconds(now.getSeconds() + 1);
             }
-            remainingTimeString += difference.getUTCMinutes();
-            remainingTimeString += " minutes, ";
-            titleSting += difference.getUTCMinutes();
-            titleSting += "m ";
+            else {
+                if (message.value != "") {
+                    document.title = message.value;
+                }
+                else {
+                    document.title = "Time!";
+                }
 
-            remainingTimeString += difference.getUTCSeconds();
-            remainingTimeString += " seconds";
-            titleSting += difference.getUTCSeconds();
-            titleSting += "s";
-
-            document.title = titleSting;
-            timerSpan.innerHTML = "Remaining time: " + remainingTimeString;
-            now.setSeconds(now.getSeconds() + 1);
+                playAudio('beepAudio');
+            }
         }
         else {
-            if (message.value != "")
-            {
-                document.title = message.value;
-            }
-            else
-            {
-                document.title = "Time!";
-            }
-
-            playAudio('beepAudio');
+            dateTime.setSeconds(dateTime.getSeconds() + 1);
+            now.setSeconds(now.getSeconds() + 1);
+            var endTimeSpan = document.getElementById('endTimeSpan');
+            endTimeSpan.innerHTML = "End time: ";
+            endTimeSpan.innerHTML += (dateTime.getHours() > 12 ? dateTime.getHours() - 12 : dateTime.getHours()) + ":";
+            endTimeSpan.innerHTML += padZeroes(dateTime.getMinutes()) + ":";
+            endTimeSpan.innerHTML += padZeroes(dateTime.getSeconds()) + (dateTime.getHours() > 12 ? " PM" : " AM");
         }
     }, 1000);
 }
 
 function resetTimer() {
     clearInterval(gTimer);
+    gPaused = false;
     stopAudio('beepAudio');
 
     var message = document.getElementById('alertMessage');
@@ -218,7 +234,7 @@ function resetTimer() {
     document.getElementById('waitingDiv').style.display = 'none';
 }
 
-function snoozeTimer(minutes=5) {
+function snoozeTimer(minutes = 5) {
     clearInterval(gTimer);
     stopAudio('beepAudio');
 
@@ -229,6 +245,16 @@ function snoozeTimer(minutes=5) {
     }
     newAlarm.setMinutes(newAlarm.getMinutes() + minutes);
     displayTime(newAlarm);
+}
+
+function pauseResumeTimer() {
+    gPaused = !gPaused;
+    var pauseResumeButton = document.getElementById('pauseResumeButton');
+    pauseResumeButton.innerHTML = pauseResumeButton.innerHTML == "Pause" ? "Resume" : "Pause";
+    if (pauseResumeButton.innerHTML == "Pause") {
+        gCurrentAlarmTime = (new Date()) + gRemainingTime;
+    }
+
 }
 
 function playAudio(id) {
